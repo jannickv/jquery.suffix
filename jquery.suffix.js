f58912e44a18ce6651b145f0eb4e8902ecadf35a
Jannick Vandaele <jannickvandaele@hotmail.com>
2015-05-07 15:13:54 +0200

    js files

---
+++ jquery.suffix.js
@@ -0,0 +1,63 @@
+$.fn.suffix = function () {
+    "use strict";
+
+    var textfields = this;
+
+    $(textfields).each(function () {
+        var instance = this;
+        var suffix = $(this).data("suffix");
+        var suffix_length = suffix.length;
+        var placeholder = $(this).data("placeholder");
+        var touched = false;
+        var textfield = $(this);
+        
+        $(textfield).val("").attr("placeholder", placeholder + suffix);
+        $(textfield).on("focus", function () {
+            if (!touched) {
+                $(textfield).val(suffix);
+                
+                setTimeout(function () {
+                    $(textfield)[0].setSelectionRange(0, 0);
+                }, 0);
+            } else {
+                var position =  $(textfield).val().length - suffix_length;
+            }            
+        });
+        
+        $(textfield).on("click", function () {
+            var position =  $(textfield).val().length - suffix_length;
+            if (this.selectionStart > position) {
+                $(textfield)[0].setSelectionRange(position, position);
+            }
+        });
+        
+        $(textfield).on("keydown change", function () {
+            touched = true;
+        });
+        
+        $(textfield).on("keyup", function () {
+            var position =  $(textfield).val().length - suffix_length;
+            if (this.selectionStart > position) {
+                $(textfield)[0].setSelectionRange(position, position);
+            }
+        });
+        
+        $(textfield).on("select", function () {
+            var position =  $(textfield).val().length - suffix_length;
+            if (this.selectionStart > position) {
+                $(textfield)[0].setSelectionRange(position, this.selectionEnd);
+            }
+            
+            if (this.selectionEnd > position) {
+                $(textfield)[0].setSelectionRange(this.selectionStart, position);
+            }
+        });
+        
+        $(textfield).blur(function () {
+            if ($(this).val() === suffix || $(this).val() === '') {
+                $(textfield).val("").attr("placeholder", placeholder + suffix);
+                touched = false;
+            }
+        });
+    });
+};
