dbe8dd6afbbd44173ccd183c1bde7c1b48146052
Jannick Vandaele <jannickvandaele@hotmail.com>
2015-05-07 15:53:31 +0200

    added function and return

--- jquery.suffix.js
+++ jquery.suffix.js
@@ -1,62 +1,68 @@
-$.fn.suffix = function () {
-    "use strict";
+(function ($) {
+    if ($.fn.suffix === undefined) {
 
-    var textfields = this;
+        $.fn.suffix = function () {
+            "use strict";
 
-    $(textfields).each(function () {
-        var instance = this;
-        var suffix = $(this).data("suffix");
-        var suffix_length = suffix.length;
+            var textfields = this;
 
-        var touched = false;
-        var textfield = $(this);
-        
-        $(textfield).on("focus", function () {
-            if (!touched) {
-                $(textfield).val(suffix);
+            $(textfields).each(function () {
+                var instance = this;
+                var suffix = $(this).data("suffix");
+                var suffix_length = suffix.length;
+
+                var touched = false;
+                var textfield = $(this);
                 
-                setTimeout(function () {
-                    $(textfield)[0].setSelectionRange(0, 0);
-                }, 0);
-            } else {
-                var position =  $(textfield).val().length - suffix_length;
-            }            
-        });
-        
-        $(textfield).on("click", function () {
-            var position =  $(textfield).val().length - suffix_length;
-            if (this.selectionStart > position) {
-                $(textfield)[0].setSelectionRange(position, position);
-            }
-        });
-        
-        $(textfield).on("keydown change", function () {
-            touched = true;
-        });
-        
-        $(textfield).on("keyup", function () {
-            var position =  $(textfield).val().length - suffix_length;
-            if (this.selectionStart > position) {
-                $(textfield)[0].setSelectionRange(position, position);
-            }
-        });
-        
-        $(textfield).on("select", function () {
-            var position =  $(textfield).val().length - suffix_length;
-            if (this.selectionStart > position) {
-                $(textfield)[0].setSelectionRange(position, this.selectionEnd);
-            }
-            
-            if (this.selectionEnd > position) {
-                $(textfield)[0].setSelectionRange(this.selectionStart, position);
-            }
-        });
-        
-        $(textfield).blur(function () {
-            if ($(this).val() === suffix || $(this).val() === '') {
-                $(textfield).val("");
-                touched = false;
-            }
-        });
-    });
-};
+                $(textfield).on("focus", function () {
+                    if (!touched) {
+                        $(textfield).val(suffix);
+                        
+                        setTimeout(function () {
+                            $(textfield)[0].setSelectionRange(0, 0);
+                        }, 0);
+                    } else {
+                        var position =  $(textfield).val().length - suffix_length;
+                    }            
+                });
+                
+                $(textfield).on("click", function () {
+                    var position =  $(textfield).val().length - suffix_length;
+                    if (this.selectionStart > position) {
+                        $(textfield)[0].setSelectionRange(position, position);
+                    }
+                });
+                
+                $(textfield).on("keydown change", function () {
+                    touched = true;
+                });
+                
+                $(textfield).on("keyup", function () {
+                    var position =  $(textfield).val().length - suffix_length;
+                    if (this.selectionStart > position) {
+                        $(textfield)[0].setSelectionRange(position, position);
+                    }
+                });
+                
+                $(textfield).on("select", function () {
+                    var position =  $(textfield).val().length - suffix_length;
+                    if (this.selectionStart > position) {
+                        $(textfield)[0].setSelectionRange(position, this.selectionEnd);
+                    }
+                    
+                    if (this.selectionEnd > position) {
+                        $(textfield)[0].setSelectionRange(this.selectionStart, position);
+                    }
+                });
+                
+                $(textfield).blur(function () {
+                    if ($(this).val() === suffix || $(this).val() === '') {
+                        $(textfield).val("");
+                        touched = false;
+                    }
+                });
+            });
+        };
+    }
+    return $.fn.suffix;
+})(jQuery);
